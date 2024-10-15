d559f2c47dc75e686d2c502e7cfb51af5172d1c1
pyctem99@gmail.com <pyctem99@gmail.com>
2024-10-16 00:43:06 +0500

    pravki zavtra to4no 5k poluchu

---
+++ Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public class Rootobject
+        {
+            public Mark mark { get; set; }
+            public Brand[] brands { get; set; }
+            public Price[] price { get; set; }
+            public object[] test { get; set; }
+        }
+
+        public class Mark
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string slug { get; set; }
+            public string is_active { get; set; }
+            public string created_at { get; set; }
+            public string deleted_at { get; set; }
+            public string user { get; set; }
+        }
+
+        public class Brand
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string slug { get; set; }
+            public string is_active { get; set; }
+            public string order { get; set; }
+            public string created_at { get; set; }
+            public string deleted_at { get; set; }
+            public string brand { get; set; }
+            public string user { get; set; }
+            public string last_price { get; set; }
+            public float percent_diff { get; set; }
+            public string price_diff { get; set; }
+            public float valute { get; set; }
+            public Price_List[] price_list { get; set; }
+        }
+
+        public class Price_List
+        {
+            public string created_at { get; set; }
+            public string _0 { get; set; }
+            public string price { get; set; }
+            public string _1 { get; set; }
+            public string country_id { get; set; }
+            public string _2 { get; set; }
+        }
+
+        public class Price
+        {
+            public string price { get; set; }
+            public string _0 { get; set; }
+            public string created_at { get; set; }
+            public string _1 { get; set; }
+            public string course { get; set; }
+            public string _2 { get; set; }
+            public string dollar { get; set; }
+            public string _3 { get; set; }
+            public string country_id { get; set; }
+            public string _4 { get; set; }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using static ConsoleApp1.Class1;
 
 namespace ConsoleApp1
 {
@@ -9,13 +10,14 @@ namespace ConsoleApp1
         {
            using(HttpClient client = new HttpClient())
             {
-                var response = await client.GetAsync("https://informer.dop.proplast.ru/api/index/pet/?moment=24%20month3&range=2024-10-09%2000:00:00,2024-10-15%2023:59:59");
+                var response = await client.GetAsync("https://informer.dop.proplast.ru/api/index/pet/?moment=24%20month3&range=2020-01-15%2000:00:00,2024-10-15%2023:59:59");
                 var responseText = await response.Content.ReadAsStringAsync();
                 Console.WriteLine(responseText);
                 int a  = responseText.LastIndexOf('>');
                 responseText = responseText.Remove(0, a + 1);
-                Root root = JsonSerializer.Deserialize<Root>(responseText);
-                var b = 0;
+                Rootobject root = JsonSerializer.Deserialize<Rootobject>(responseText);
+                Console.WriteLine();
+                Console.ReadKey();
             }
         }
     }
